3bd8c266beb4d3ac472660f2c01b1230ff4f87e9
ethan6077 <ethan6077@gmail.com>
2019-05-25 21:49:48 +1000

    added StatusMap to match status string

--- src/components/property/PropertyStatus.js
+++ src/components/property/PropertyStatus.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import SmallCircle from './SmallCircle';
+import StatusMap from '../header/StatusMap';
 import styles from './PropertyStatus.module.css';
 
 class PropertyStatus extends React.Component {
   render() {
     const { status } = this.props;
+    const statusValue = StatusMap.get(status);
     return (
       <div>
-        <span className={styles.text}>{status}</span>
+        <span className={styles.text}>{statusValue}</span>
         <SmallCircle status={status} />
       </div>
     );
--- src/components/property/SmallCircle.js
+++ src/components/property/SmallCircle.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './SmallCircle.module.css';
 
 class SmallCircle extends React.Component {
   render() {
     const { status } = this.props;
     return (
-      <span className={styles.dot}></span>
+      <span className={classNames({
+        [styles.current]: status === 'current',
+        [styles.sold]: status === 'sold',
+        [styles.offMarket]: status === 'off_market',
+        [styles.withDrawn]: status === 'withdrawn',
+      })}></span>
     );
   }
 }
