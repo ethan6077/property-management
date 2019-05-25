d918ea4afe5ca680873274403afa9c555b635be8
ethan6077 <ethan6077@gmail.com>
2019-05-25 19:37:04 +1000

    added numeral for currency format

--- src/components/property/Price.js
+++ src/components/property/Price.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import numeral from 'numeral';
 import styles from './Price.module.css';
 
 class Price extends React.Component {
   render() {
     const { price } = this.props;
+    const priceStr = numeral(price).format('$0,0');
     return (
       <div className={styles.container}>
-        {price}
+        {priceStr}
       </div>
     );
   }
