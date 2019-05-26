dadcd67a6d5d1020cd79a6af3dc2217ad276dab1
ethan6077 <ethan6077@gmail.com>
2019-05-26 19:57:59 +1000

    Testing the filtered property list done

--- src/components/PropertyPage.test.js
+++ src/components/PropertyPage.test.js
@@ -35,3 +35,46 @@ it('renders HeaderContainer and PropertyListContainer when propertyStatus is don
   expect(propertyPageWrapper.find(HeaderContainer)).to.have.lengthOf(1);
   expect(propertyPageWrapper.find(PropertyListContainer)).to.have.lengthOf(1);
 });
+
+it('renders filtered propertyList when propertyFilter is set', () => {
+  const propertyPageWrapper = shallow(<PropertyPage />, { disableLifecycleMethods: true });
+  propertyPageWrapper.setState({ propertyList: [
+    {
+      "id": 1,
+      "status": "current",
+      "street": "1 Fake Street",
+      "suburb": "Glen Iris",
+      "state": "VIC",
+      "postcode": "3146",
+      "price": 1230500,
+      "image": "https://code-challenge.activepipe.com/property-images/frontage/01.jpg"
+    },
+    {
+      "id": 2,
+      "status": "sold",
+      "street": "2 Fake Street",
+      "suburb": "Glen Iris",
+      "state": "VIC",
+      "postcode": "3146",
+      "price": 1330500,
+      "image": "https://code-challenge.activepipe.com/property-images/frontage/02.jpg"
+    },
+    {
+      "id": 3,
+      "status": "current",
+      "street": "3 Fake Street",
+      "suburb": "Glen Iris",
+      "state": "VIC",
+      "postcode": "3146",
+      "price": 2200000,
+      "image": "https://code-challenge.activepipe.com/property-images/frontage/03.jpg"
+    },
+  ] });
+  propertyPageWrapper.setState({ propertyStatus: 'done' });
+  expect(propertyPageWrapper.find(PropertyListContainer)).to.have.lengthOf(1);
+  // by default, list all the properties
+  expect(propertyPageWrapper.find(PropertyListContainer).props().propertyList).to.have.lengthOf(3);
+  // after set the filter, only available properties will be listed
+  propertyPageWrapper.setState({ propertyFilter: 'current' });
+  expect(propertyPageWrapper.find(PropertyListContainer).props().propertyList).to.have.lengthOf(2);
+});
