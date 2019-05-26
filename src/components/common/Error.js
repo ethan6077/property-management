f2562d78e7c79ee73da9a643e1b7e25758c01ad6
ethan6077 <ethan6077@gmail.com>
2019-05-26 16:44:09 +1000

    Error module done

--- src/components/common/Error.js
+++ src/components/common/Error.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './Error.module.css';
+import SadFace from '../../img/sad-face.svg';
+import RefreshBtn from '../../img/refresh.svg';
 
 function Error() {
   return (
     <div className={styles.error}>
-      <div>
-        <span>FACE</span>
-        <span>Uh-oh... it looks like some things haven't loaded correctly.</span>
-      </div>
-      <span>REFRESH BUTTON</span>
+      <section>
+        <img src={SadFace} alt="sad-face" />
+        <span className={styles.text}>Uh-oh... it looks like some things haven't loaded correctly.</span>
+      </section>
+      <img className={styles.refreshBtn} src={RefreshBtn} alt="sad-face" onClick={() => window.location.reload(true)} />
     </div>
   );
 }
